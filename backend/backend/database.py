97dbb8b336149fb2b26d5461fc3dae755f1e1734
turysta11 <info@turystatravel.com>
2026-07-18 14:18:55 -0400

    Update database.py

--- backend/backend/database.py
+++ backend/backend/database.py
@@ -1,11 +1,12 @@
 import os
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Railway automatically provides the DATABASE_URL when you link Postgres
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:password@localhost/hoteltracker")
 
 engine = create_engine(DATABASE_URL)
+Base = declarative_base()
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def get_db():
